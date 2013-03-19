764b0b66699217529bc54891cf54357fa10724dc
Marek Otahal <markotahal@gmail.com>
2013-03-19 16:42:44 +0100

    formating

--- src/test/java/nl/vanrijn/pooler/TemporalPoolerTest.java
+++ src/test/java/nl/vanrijn/pooler/TemporalPoolerTest.java
@@ -3,10 +3,9 @@ package nl.vanrijn.pooler;
 import nl.vanrijn.model.Cell;
 import nl.vanrijn.model.LateralSynapse;
 import nl.vanrijn.model.Segment;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class TemporalPoolerTest {
 
@@ -23,7 +22,7 @@ public class TemporalPoolerTest {
         Cell cell = tempo.getCells()[0][0][Cell.NOW];
         Segment segment = cell.getSegments().get(0);
 
-        for(LateralSynapse synaps : segment.getSynapses()) {
+        for (LateralSynapse synaps : segment.getSynapses()) {
             Cell cell1 = tempo.getCells()[synaps.getFromColumnIndex()][synaps.getFromCellIndex()][Cell.NOW];
             cell1.setActiveState(true);
             cell1.setLearnState(true);
@@ -32,15 +31,15 @@ public class TemporalPoolerTest {
 
         Segment segment2 = cell.getSegments().get(1);
         segment2.setSequenceSegment(true);
-        for(LateralSynapse synaps : segment2.getSynapses()) {
+        for (LateralSynapse synaps : segment2.getSynapses()) {
             Cell cell1 = tempo.getCells()[synaps.getFromColumnIndex()][synaps.getFromCellIndex()][Cell.NOW];
             cell1.setActiveState(true);
             cell1.setLearnState(true);
         }
         Segment segmentToTest = tempo.getActiveSegment(0, 0, Cell.NOW, Cell.ACTIVE_STATE);
-        System.out.println(this.getClass().getName()+">> segment connected synapses = "+segment.getConnectedSynapses().size());
-        System.out.println(this.getClass().getName()+">> "+tempo.segmentActive(segment, Cell.NOW, Cell.ACTIVE_STATE));
-        System.out.println(this.getClass().getName()+">> is Ssequence segment: "+segmentToTest.isSequenceSegment());
+        System.out.println(this.getClass().getName() + ">> segment connected synapses = " + segment.getConnectedSynapses().size());
+        System.out.println(this.getClass().getName() + ">> " + tempo.segmentActive(segment, Cell.NOW, Cell.ACTIVE_STATE));
+        System.out.println(this.getClass().getName() + ">> is Sequence segment: " + segmentToTest.isSequenceSegment());
         assertEquals(segmentToTest.isSequenceSegment(), false);
     }
 }
