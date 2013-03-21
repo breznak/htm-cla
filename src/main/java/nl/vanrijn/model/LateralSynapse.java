516d2bb2f812fee97921a9ce54cb9a854b3f79bb
Marek Otahal <markotahal@gmail.com>
2013-03-22 00:46:30 +0100

    rename SynapseAbstract to LateralSynapse
    and LateralSynapse now contains all the functionality

--- src/main/java/nl/vanrijn/model/LateralSynapse.java
+++ src/main/java/nl/vanrijn/model/LateralSynapse.java
@@ -3,21 +3,68 @@
  */
 package nl.vanrijn.model;
 
-public class LateralSynapse extends SynapseAbstract {
+import nl.vanrijn.utils.HelperMath;
 
-    private final Cell fromCell;
+/**
+ * synapse A data structure representing a synapse - contains a permanence value
+ * and the source input index.
+ *
+ * @author vanrijn
+ */
+public class LateralSynapse<T> {
 
-    public LateralSynapse(Cell from, double initialPerm) {
-        super(initialPerm);
+    /**
+     * T fromCell ...Cell for dendrite in TemporalPooler, Column in
+     * SpatialPooler
+     */
+    private final T fromCell;
+    /**
+     * similar like weight in NN ; range 0..1
+     */
+    private double permanance;
+
+    public LateralSynapse(T from, double initialPerm) {
+        this.permanance = initialPerm;
         this.fromCell = from;
     }
 
-    public Cell getFromCell() {
+    public T getFromCell() {
         return fromCell;
+    }
+
+    /**
+     * synapse is considered connected if its permanance is bigger than
+     * connectedPermanance
+     *
+     * @param connectedPermanance
+     * @return
+     */
+    public boolean isConnected(double connectedPermanance) {
+        // logger.log(Level.INFO, "synapse perm ="+this.permanance +" "+(this.permanance>=CONECTED_PERMANANCE)+
+        // "input="+sourceInput);
+        return this.permanance >= connectedPermanance;
+    }
+
+    /**
+     * permanance is like weight in NN
+     *
+     * @return
+     */
+    public double getPermanance() {
+        return this.permanance;
+    }
+
+    /**
+     * set permanence value to d, ensure range is between 0..1
+     *
+     * @param d
+     */
+    public void setPermanance(double d) {
+        this.permanance = HelperMath.inRange(d, 0, 1);
     }
 
     @Override
     public String toString() {
-        return "LateralSynapse from " + this.fromCell + ",perm " + getPermanance();
+        return this.getClass().getSimpleName() + " from " + this.fromCell + ",perm " + getPermanance();
     }
 }
